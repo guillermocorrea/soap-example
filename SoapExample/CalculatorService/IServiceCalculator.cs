10b5d23179ec762c8e8d1e24b8cc9616efd2205f
Guillermo Correa <luis.guillermo.correa@hotmail.com>
2014-09-07 09:35:23 -0500

    added CalculatorService

---
+++ SoapExample/CalculatorService/IServiceCalculator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace CalculatorService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IServiceCalculator" in both code and config file together.
+    [ServiceContract]
+    public interface IServiceCalculator
+    {
+        /// <summary>
+        /// Sums the specified first number.
+        /// </summary>
+        /// <param name="firstNumber">The first number.</param>
+        /// <param name="secondNumber">The second number.</param>
+        /// <returns></returns>
+        [OperationContract]
+        double Sum(double firstNumber, double secondNumber);
+
+        /// <summary>
+        /// Subtracts the specified first number.
+        /// </summary>
+        /// <param name="firstNumber">The first number.</param>
+        /// <param name="secondNumber">The second number.</param>
+        /// <returns></returns>
+        [OperationContract]
+        double Subtract(double firstNumber, double secondNumber);
+    }
+}
---
+++ SoapExample/CalculatorService/ServiceCalculator.svc.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace CalculatorService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "ServiceCalculator" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select ServiceCalculator.svc or ServiceCalculator.svc.cs at the Solution Explorer and start debugging.
+    public class ServiceCalculator : IServiceCalculator
+    {
+        /// <summary>
+        /// Sums the specified first number.
+        /// </summary>
+        /// <param name="firstNumber">The first number.</param>
+        /// <param name="secondNumber">The second number.</param>
+        /// <returns></returns>
+        public double Sum(double firstNumber, double secondNumber)
+        {
+            return firstNumber + secondNumber;
+        }
+
+        /// <summary>
+        /// Subtracts the specified first number.
+        /// </summary>
+        /// <param name="firstNumber">The first number.</param>
+        /// <param name="secondNumber">The second number.</param>
+        /// <returns></returns>
+        public double Subtract(double firstNumber, double secondNumber)
+        {
+            return firstNumber - secondNumber;
+        }
+    }
+}
